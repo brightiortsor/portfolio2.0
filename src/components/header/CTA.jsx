9eb388f23ea0a207b3180bd4a69821530769ce43
Bright Iortsor <iortsor@gmail.com>
2023-01-23 13:49:59 +0100

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CV from "../../assets/IORTSOR-BRIGHT-FE.pdf";
+import CV from "../../assets/BRIGHT-IORTSOR-0FE.pdf";
 const CTA = () => {
   return (
     <div className="cta">
