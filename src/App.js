724f0adcc703425a17b61eea130354ab463b5db2
Bright <iortsor@gmail.com>
2023-03-03 05:33:05 +0100

    added loading spinner

--- src/App.js
+++ src/App.js
@@ -2,20 +2,49 @@ import Header from "./components/header/Header.jsx";
 import About from "./components/about/About.jsx";
 import Navbar from "./components/navbar/Navbar.jsx";
 import Projects from "./components/projects/Projects.jsx";
-// import Testimonial from "./components/testimonials/Testimonial.jsx";
 import Contact from "./components/contact/Contact.jsx";
 import Experience from "./components/experience/Experience.jsx";
 import Footer from "./components/footer/Footer.jsx";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      startEvent: "DOMContentLoaded",
+      offset: 0,
+      once: true,
+    });
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  return loading ? (
+    <div className="loading">
+      <div className="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  ) : (
     <div>
       <Header />
       <About />
       <Navbar />
       <Experience />
       <Projects />
-      {/* <Testimonial /> */}
       <Contact />
       <Footer />
     </div>
